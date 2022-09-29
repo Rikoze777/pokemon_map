22622572900dc03af6e918d0ccfd682c2ed2415f
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2022-09-29 21:20:58 +0300

    register Pokemon model on admin UI

--- pokemon_entities/admin.py
+++ pokemon_entities/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Pokemon
+from .models import Pokemons
 
 
-admin.site.register(Pokemon)
+admin.site.register(Pokemons)
--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Pokemon(models.Model):
+class Pokemons(models.Model):
     title = models.CharField(max_length=200)
 
     def __str__(self):
